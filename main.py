8947a9181320d699e6c418db9d307668846fd255
herunr0 <herunurrohman20@gmail.com>
2024-09-27 11:03:17 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
-from typing import List, Optional
+import random
+
+print("=================================")
+print("=================================")
+print ("selamat datang di game menggembul")
+print("=================================")
+print("=================================")
 
 
-def main(args: Optional[List[str]] = None) -> int:
-    """This is preserved for old console scripts that may still be referencing
-    it.
 
-    For additional details, see https://github.com/pypa/pip/issues/7498.
-    """
-    from pip._internal.utils.entrypoints import _wrapper
+nama_user = "tata"
+nama_user=input("siapakah nama kamu?")
 
-    return _wrapper(args)
+print(f"halo selamat datang {nama_user}!!!")
+
+nomorkucing = random.randint(1,5)
+kucing = "(^v^)"
+semua_kucing = [kucing] * 5
+kucing_benar = semua_kucing.copy()
+
+
+kucing_benar[nomorkucing - 1] = "0v0"
+
+print(f'''selamat datang {nama_user} coba perhatikan gambar kucing dibawah ini? 
+      {semua_kucing}''')
+
+jawaban= int(input("dari kucing diatas, kucing mana yang paling gembul[1/2/3/4/5]?"))
+
+
+konfirmasi = input(f"apakah kamu yakin jawabannya {jawaban} ?[y/n]")
+
+
+if konfirmasi == "n":
+    print("program dihentikan")
+    exit
+elif konfirmasi == "y":
+    if jawaban == nomorkucing:
+        print(f'''selamat anda benar, kegembulan ada di kucing nomor {nomorkucing}
+              {kucing_benar}''')
+    else :
+        print(f'''yah salah, ternyata kucing yg gembul adalah kucing nomor {nomorkucing}
+              {kucing_benar} sedangkan jawaban kamu adalah kucing nomor {jawaban}''')
+else:
+    print("silahkan ulangi")
